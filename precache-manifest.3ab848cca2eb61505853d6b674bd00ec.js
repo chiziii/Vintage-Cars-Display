2fe79886b0ee4c248b173c067d290b023fcbb03e
Chiziii <igwechizy@gmail.com>
2020-05-19 02:52:40 +0100

    Updates

---
+++ precache-manifest.3ab848cca2eb61505853d6b674bd00ec.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "771c60b4410802c8acd31b20049a9f3d",
+    "url": "/Vintage-Cars-Display/index.html"
+  },
+  {
+    "revision": "96de981c7d237ae3bcd9",
+    "url": "/Vintage-Cars-Display/static/css/2.daea5752.chunk.css"
+  },
+  {
+    "revision": "657be074f4cbf151879b",
+    "url": "/Vintage-Cars-Display/static/css/main.049f000e.chunk.css"
+  },
+  {
+    "revision": "96de981c7d237ae3bcd9",
+    "url": "/Vintage-Cars-Display/static/js/2.7be41377.chunk.js"
+  },
+  {
+    "revision": "1c0c8417523bf3c55484c1c5efb74a72",
+    "url": "/Vintage-Cars-Display/static/js/2.7be41377.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "657be074f4cbf151879b",
+    "url": "/Vintage-Cars-Display/static/js/main.940e6836.chunk.js"
+  },
+  {
+    "revision": "8b9eef8f6de35d35b7fa",
+    "url": "/Vintage-Cars-Display/static/js/runtime-main.0399b82f.js"
+  },
+  {
+    "revision": "51d79b91ca3596ee650958109ab2f0e0",
+    "url": "/Vintage-Cars-Display/static/media/Bodoni BE Oldstyle Figures Bold.51d79b91.otf"
+  },
+  {
+    "revision": "be89a5bed739eee8649fa3322ff5c52b",
+    "url": "/Vintage-Cars-Display/static/media/Engravers-Gothic-BT-Font.be89a5be.ttf"
+  },
+  {
+    "revision": "302a581b616b098be38a14e17dbf91c4",
+    "url": "/Vintage-Cars-Display/static/media/Orange.302a581b.jpg"
+  },
+  {
+    "revision": "1ddf37388ecf1851228874bda2fc63d7",
+    "url": "/Vintage-Cars-Display/static/media/burntOrange.1ddf3738.jpg"
+  },
+  {
+    "revision": "0d6cb5bb35cf3ab966b9bc6b9d363329",
+    "url": "/Vintage-Cars-Display/static/media/yellow.0d6cb5bb.png"
+  }
+]);
